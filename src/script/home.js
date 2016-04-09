283b42f4ba6cad6a0a6e656f386e70b62932218d
AlejoNext <alejonext@gmail.com>
2016-04-09 14:18:05 -0500

    Ok!

--- src/script/home.js
+++ src/script/home.js
@@ -4,25 +4,34 @@ export class f {
 		'$scope',
 		'ngDialog',
 		'$http',
-		'url'
+		'url',
+		'geolocation'
 	];
-	constructor(scope, ngdilalog, http, name) {
+	constructor(scope, ngdilalog, http, name, geo) {
 		this.scope = scope;
 		this.dialog = ngdilalog;
 		this.http = http.post;
 		this._url = name;
-	}
-
-	sender (){
-		this.http(this._url, this.data).then((err) => {
-			this.dialog.open({
-				template: 'externalTemplate.html',
-				scope: this.scope
-			});
+		this.geo  = 'SIN UBICACION';
+		geo.getLocation().then(data =>{
+			this.geo = `lat:${data.coords.latitude},long:${data.coords.longitude}`
 		});
 	}
 
-	close(){
-		this.dialog.closeAll();
+	sender (){
+		this.http(`${this._url}${this.class}/with/key/WM_9WK0RoXywnW0harCb_`, {
+			'value1' : `${this.name} <${this.email}>`,
+			'value2' : this.movil,
+			'value3' : this.geo,
+		}).then(() => {
+			this.name = '';
+			this.movil = '';
+			this.email = '';
+			this.class = '';
+
+			this.dialog.open({
+				template : 'ok.html',
+			});
+		});
 	}
 }
--- src/script/lib.js
+++ src/script/lib.js
@@ -1,11 +1,13 @@
 import 'angular';
 import 'angular-messages';
 import 'ng-dialog';
+import 'angularjs-geolocation';
 import * as h from './home.js';
 import * as a from './minH.js';
 
+
 angular
-	.module('open', [ 'ng', 'ngMessages', 'ngDialog'])
+	.module('open', [ 'ng', 'ngMessages', 'ngDialog', 'geolocation'])
 	.controller(h.name, h.f)
 	.directive(a.name, a.f)
-	.constant('url', 'http');
+	.constant('url', 'https://maker.ifttt.com/trigger/');
